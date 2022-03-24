99f4825fc399e08588f6c8f361a4c858827c9fe2
Richard Viana <richardpassostk@gmail.com>
2022-03-24 17:03:37 -0500

    adicionada função javascript de scroll no header

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var logCadRouter = require('./routes/logCad');
 var equipeRouter = require('./routes/equipe')
 
 var app = express();
@@ -23,17 +22,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/cadastro', logCadRouter);
 app.use('/equipe', equipeRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
